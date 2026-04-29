a471de0f068f9b8cfab134e130ed567ba13c2be8
Sebastian Heinzenburger <sebastian.heinzenburger@sap.com>
2026-04-29 08:58:31 +0200

    `to_string` applied to a type that implements `Display` in `write!` args

--- 01_domain/src/peer.rs
+++ 01_domain/src/peer.rs
@@ -7,7 +7,7 @@ pub struct PeerAddress(Arc<str>);
 
 impl Display for PeerAddress {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0.to_string())
+        write!(f, "{}", self.0)
     }
 }
 
